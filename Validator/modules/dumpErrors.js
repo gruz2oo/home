02720555db5473dfb38c33ee6984b716ad774b35
Valery <63974753+gruz2oo@users.noreply.github.com>
2020-11-11 05:12:46 +0300

    Create dumpErrors.js

---
+++ Validator/modules/dumpErrors.js
@@ -0,0 +1,9 @@
+export {dumpErrors};
+
+function dumpErrors(errorList, validatedObject) {
+    errorList.forEach((error) => {
+        console.log(`Field: ${error.key}`);
+        console.log(`Provided value: ${validatedObject[error.key]}`);
+        console.log(`Message: ${error.message}`);
+    });
+}
